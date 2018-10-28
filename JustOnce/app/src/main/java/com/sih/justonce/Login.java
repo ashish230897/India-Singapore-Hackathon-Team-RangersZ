9cda5977a2dfb452b91a306873ef4e31dbd37440
sidgangan <sgangan298@gmail.com>
2018-10-28 14:18:57 +0530

    added AppCompat

--- JustOnce/app/src/main/java/com/sih/justonce/Login.java
+++ JustOnce/app/src/main/java/com/sih/justonce/Login.java
@@ -2,8 +2,9 @@ package com.sih.justonce;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Login extends Activity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
